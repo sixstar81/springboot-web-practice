69303bbeee088dd00a0b4bf26bbf7ff9e99209df
이상국 <sixstar6@dbgroup.co.kr>
2024-11-26 08:48:43 +0900

    test

--- src/main/java/com/sk/practice/PracticeApplication.java
+++ src/main/java/com/sk/practice/PracticeApplication.java
@@ -1,12 +1,22 @@
 package com.sk.practice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+		};
 	}
 }
--- src/main/java/com/sk/practice/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sk.practice.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-    
-}
