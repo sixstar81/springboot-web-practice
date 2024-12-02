cb32cda31d509be9360a3db7877f2a7d9a1cb832
이상국 <sixstar6@dbgroup.co.kr>
2024-12-03 08:14:12 +0900

    issue controller teest2

--- src/main/java/com/sk/practice/issue/controller/IssueController.java
+++ src/main/java/com/sk/practice/issue/controller/IssueController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,6 +27,27 @@ public class IssueController {
         System.out.println("request: " + request);
 
         List<Attachment> saveFiles = fileService.saveFiles(request.getFiles());
+        Issue entity = request.toEntity();
+
+        entity.addAttachments(
+            saveFiles
+        );
+
+        Long issueId = issueService.createIssue(entity);
+
+        return ResponseEntity.ok(issueId);
+    }
+
+    @PostMapping(
+        value = "/json",    
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Long> createIssue2(
+        @RequestPart("data") IssueRequest request,
+        @RequestPart("files") List<MultipartFile> files
+        ) throws IOException {
+        System.out.println("request: " + request);
+
+        List<Attachment> saveFiles = fileService.saveFiles(files);
         Issue entity = request.toEntity();
 
         entity.addAttachments(
